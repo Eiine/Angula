e77cae2950b18d1cfd484e7a24f57020a20dd16a
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-12 18:04:41 -0300

    zumvido v2

--- api/src/public/zumbido.js
+++ api/src/public/zumbido.js
@@ -3,5 +3,6 @@ const audio = new Audio("../audio/1.mp3");
 boton.addEventListener("click", () => {
   console.log("hola mundo");
   audio.play();
-  navigator.vibrate(1000);
+  console.log(navigator.vibrate);
+  window.navigator.vibrate(1000);
 });
