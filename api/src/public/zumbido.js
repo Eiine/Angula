633b52aa876d50a218fea57ed9c3760cd8e69e00
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-13 16:38:15 -0300

    revisando funcionalidad de chat

--- api/src/public/zumbido.js
+++ api/src/public/zumbido.js
@@ -1,13 +1,7 @@
 let boton = document.getElementById("zumbido");
 const audio = new Audio("../audio/1.mp3");
-navigator.vibrate =
-  navigator.vibrate ||
-  navigator.webkitVibrate ||
-  navigator.mozVibrate ||
-  navigator.msVibrate;
 boton.addEventListener("click", () => {
   console.log("hola mundo");
   audio.play();
-  console.log(navigator.vibrate);
-  navigator.vibrate([300, 100, 300, 100, 300]);
+  window.navigator.vibrate([100]);
 });
